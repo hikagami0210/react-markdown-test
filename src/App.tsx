8d24e1cd920f5b3354b94710b91a476f3280d153
hikagami0210 <fsban772@gmail.com>
2024-12-23 12:01:58 +0900

    feat: スタイルをあてる

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [markdown, setMarkdown] = useState<string>("");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>react-markdown</h1>
+      <h2>Markdown 入力</h2>
+      <textarea
+        placeholder="マークダウンを入力してください"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+
+      <h2>プレビュー</h2>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
+    </div>
+  );
 }
 
-export default App
+export default App;
